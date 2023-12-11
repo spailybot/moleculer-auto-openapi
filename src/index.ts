f7fe273f53919a7f541983fc4ebb8a63212c04a2
thibaut severac <thib3113@gmail.com>
2023-12-11 10:57:49 +0100

    feat(filterAliases): improve typescript exports

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@
 
 import { OPENAPI_VERSIONS_SUPPORTED } from './constants.js';
 import { mixin } from './mixin.js';
+import { Alias } from './objects/Alias.js';
+import { Route } from './objects/Route.js';
+import { PathAction } from './objects/PathAction.js';
+import { HTTP_METHODS, rawHttpMethod, JOKER_METHOD } from './constants.js';
 import type * as MoleculerWebTypes from './types/moleculer-web.js';
 import { OA_GENERATE_DOCS_INPUT, OA_GENERATE_DOCS_OUTPUT } from './types/openapi.js';
 
@@ -18,4 +22,15 @@ export default mixin;
 export { mixin };
 export const OpenApiMixin = mixin;
 
-export { OA_GENERATE_DOCS_INPUT, OA_GENERATE_DOCS_OUTPUT, OPENAPI_VERSIONS_SUPPORTED, MoleculerWebTypes };
+export {
+    OA_GENERATE_DOCS_INPUT,
+    OA_GENERATE_DOCS_OUTPUT,
+    OPENAPI_VERSIONS_SUPPORTED,
+    MoleculerWebTypes,
+    Alias,
+    Route,
+    HTTP_METHODS,
+    PathAction,
+    rawHttpMethod,
+    JOKER_METHOD
+};
